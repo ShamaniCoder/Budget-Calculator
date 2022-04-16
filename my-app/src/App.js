37da725cfdb881315b4ce96947a220426e02ce1f
ShamaniCoder <ertugrulaktn@gmail.com>
2022-04-16 14:36:48 +0200

    added basic components

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+
+const initialExpenses = [
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  {
+    id: uuidv4(),
+    charge: "credit card bill ",
+    amount: 1200,
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  return (
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+      <ExpenseForm />
+      <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending : <span className="total">
+          $ {expenses.reduce((acc, curr)=>{
+            return acc += curr.amount
+          },0)}
+        </span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
